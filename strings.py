a893fc12b1140a18fe81b6fc60f419ace11d61c5
Bagdat <b_batzhan@kbtu.kz>
2024-01-21 21:19:00 +0600

    kn

---
+++ strings.py
@@ -0,0 +1,27 @@
+#exercise1
+x = "Hello World"
+print(len(x))
+
+#exercise2
+txt = "Hello World"
+txt= txt[0]
+
+#exercise3   
+txt = "Hello World"
+txt = txt[2:5]
+
+#exercise4
+txt = "Hello World"
+txt = txt.strip()
+
+#Exercise5
+txt = "Hello World"
+txt = txt.upper()
+
+#exercise6
+txt = "Hello World"
+txt = txt.lower()
+
+#exercise7
+txt = "Hello World"
+txt = txt.replace("H", "J")
