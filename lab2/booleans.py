9b33e79dcef4ad2fb5fbc04710c2fb6f4ea3669e
Zenthonn <156891752+Zenthonn@users.noreply.github.com>
2024-01-25 22:05:14 +0600

    Add files via upload

---
+++ lab2/booleans.py
@@ -0,0 +1,19 @@
+#exercise1
+print(10 > 9)
+True
+
+#exercise2
+print(10 == 9)
+False
+
+#exercise3
+print(10 < 9) 
+False
+
+#exercise4
+print(bool("abc"))
+True
+
+#exercise5
+print(bool(0))
+False
